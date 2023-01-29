84212b8759ede3aa401f5345aa96dd5c79e30fbc
robsoninoue <robson.inoue@gmail.com>
2023-01-29 12:19:23 -0300

    created vagrant

---
+++ docker-vagrant/docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/cocker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
